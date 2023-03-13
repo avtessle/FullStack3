fade65b1177fe63a92d33bbf7d778cab0f134657
Nevo Cohen <nevcohen@g.jct.ac.il>
2023-03-13 23:01:01 +0200

    edit button

--- scripts/requests.js
+++ scripts/requests.js
@@ -46,21 +46,28 @@ function seeAll(){
 //See a specific record
 function seeRecord(date,time){
     let request=new FXMLHttpRequest();    
-    request.onload= function() {
-        let showRecText=document.getElementById("showRec");
+    request.onload= function() {;
         if (this.status == 200) {
-            recVisibility('visible');
-            showRecText.innerHTML = request.responseText;  
+            removeRec();
             observedRec= request.responseText;
-            //let rec=JSON.parse(request.responseText); 
+            displayRec(JSON.parse(observedRec));
+            recVisibility('visible');
         }
         else{
             alert("There is no matching appointment!");
-            recVisibility('hidden');
+            removeRec();
         }
     };
     request.open("GET", `appointment/single/${date}/${time}`);
     request.send();
+}
+
+function removeRec(){
+    if(observedRec){
+        document.getElementById("recTable").remove();
+        document.getElementById("submitEditBtn").remove();
+        recVisibility('hidden');
+    }
 }
 
 function seeDateRecord(date){
@@ -75,7 +82,7 @@ function seeDateRecord(date){
         }
         else{
             alert("There is no matching appointment!");
-            // recVisibility('hidden');
+                //recVisibility('hidden');
         }
     };
     request.open("GET", `appointment/multi/date/${date}`);
@@ -95,7 +102,52 @@ function add(newRecord){
 }
 
 function editRecord(){
-    alert("appointment deleted");
+    let table=document.getElementById("recTable");
+    let cells = table.getElementsByTagName('td');
+    for (let i = 1; i < cells.length; i+=2) {
+        cells[i].addEventListener('click', editCell);
+        cells[i].classList.add('editable');
+    }
+
+    // Create a button element
+    let button = document.createElement("button");
+
+    // Set the button text and attributes
+    button.innerText = "submit changes";
+    button.setAttribute("type", "button");
+    button.setAttribute("id", "submitEditBtn");
+
+    // Append the button to the HTML document
+    document.getElementById("searchDiv").appendChild(button);
+    button.addEventListener('click',submitChanges);
+}
+
+function submitChanges(){
+
+}
+
+function editCell(event) {
+    // Get the current value of the cell
+    let currentValue = event.target.innerHTML;
+
+    // Replace the cell content with an input field
+    event.target.innerHTML = `<input type="text" value="${currentValue}">`;
+
+    // Focus on the input field and select its content
+    let input = event.target.getElementsByTagName('input')[0];
+    input.focus();
+    input.select();
+
+    // Add an event listener to the input field
+    input.addEventListener('blur', inputBlur);
+}
+
+function inputBlur(event) {
+    // Get the new value from the input field
+    let newValue = event.target.value;
+
+    // Replace the input field with the new value
+    event.target.parentNode.innerHTML = newValue;
 }
 
 function deleteRecord(){
@@ -104,7 +156,7 @@ function deleteRecord(){
         if (this.status == 200) {
             alert("deleted!");
             seeAll();
-            recVisibility('hidden');
+            removeRec();
         }
     };
     request.open("DELETE", "appointment/delete",observedRec);
@@ -112,7 +164,7 @@ function deleteRecord(){
 }
 
 function recVisibility(mode){
-    document.getElementById("showRec").style.visibility = mode;
+    //document.getElementById("showRec").style.visibility = mode;
     document.getElementById("editBtn").style.visibility = mode;
     document.getElementById("deleteBtn").style.visibility = mode;
 }
@@ -132,4 +184,28 @@ function insertMeet(myMeets){
         hour.appendChild(phone);
     });
    
+}
+
+function displayRec(rec){
+
+    let table = document.createElement('table');
+    table.id = 'recTable';
+
+    // Loop through the record properties and create a row for each one
+    for (let key in rec) {
+        let row = document.createElement('tr');
+
+        // Create a cell for the property name
+        let nameCell = document.createElement('td');
+        nameCell.innerText = key;
+        row.appendChild(nameCell);
+
+        // Create a cell for the property value
+        let valueCell = document.createElement('td');
+        valueCell.innerText = rec[key];
+
+        row.appendChild(valueCell);
+        table.appendChild(row);
+    }
+    document.getElementById("searchDiv").appendChild(table);
 }
